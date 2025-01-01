b6ba799a18f95bc3668747394a16b3a6ee0cb623
cawzkf <camilafelixdosreis@gmail.com>
2025-01-01 15:02:41 -0400

    Ajuste com mapOf

--- app/src/main/java/com/ecoactivity/calculadora_cientifica/MainActivity.kt
+++ app/src/main/java/com/ecoactivity/calculadora_cientifica/MainActivity.kt
@@ -20,61 +20,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        var visor = findViewById<TextView>(R.id.tvResultado)
-        var num0 = findViewById<Button>(R.id.btn0)
-        var num1 = findViewById<Button>(R.id.btn1)
-        var num2 = findViewById<Button>(R.id.btn2)
-        var num3 = findViewById<Button>(R.id.btn3)
-        var num4 = findViewById<Button>(R.id.btn4)
-        var num5 = findViewById<Button>(R.id.btn5)
-        var num6 = findViewById<Button>(R.id.btn6)
-        var num7 = findViewById<Button>(R.id.btn7)
-        var num8 = findViewById<Button>(R.id.btn8)
-        var num9 = findViewById<Button>(R.id.btn9)
-        var parenteses = findViewById<Button>(R.id.btnAbreFecha)
-        var virgula = findViewById<Button>(R.id.btnDecimal)
-        var igual = findViewById<Button>(R.id.btnIgual)
-        var divisao = findViewById<Button>(R.id.btnDivisao)
-        var multiplicacao = findViewById<Button>(R.id.btnMultiplicacao)
-        var subtrair = findViewById<Button>(R.id.btnSubtracao)
-        var somar = findViewById<Button>(R.id.btnSoma)
-        var backspace = findViewById<Button>(R.id.btnBack)
-        var porcentagem = findViewById<Button>(R.id.btnPorcentagem)
-        var limpar = findViewById<Button>(R.id.btnLimparTudo)
+        val visor = findViewById<TextView>(R.id.tvResultado)
+        val buttons = mapOf(
+            "0" to R.id.btn0, "1" to R.id.btn1, "2" to R.id.btn2,"3" to R.id.btn3,
+            "4" to R.id.btn4, "5" to R.id.btn5, "6" to R.id.btn6, "7" to R.id.btn7,
+            "8" to R.id.btn8, "9" to R.id.btn9, "+" to R.id.btnSoma, "÷" to R.id.btnDivisao,
+            "-" to R.id.btnSubtracao, "x" to R.id.btnMultiplicacao
+        )
 
-        divisao.setOnClickListener { visor.append("÷") }
-        multiplicacao.setOnClickListener { visor.append("×") }
-        subtrair.setOnClickListener { visor.append("-") }
-        somar.setOnClickListener { visor.append("+") }
 
-        backspace.setOnClickListener {
-            val legth = visor.text.length
-            if (legth > 0){
-                visor.text = visor.text.subSequence(0,legth-1)
-            }
-        }
 
-        limpar.setOnClickListener {
-            val leght = visor.text.length
-            if(leght > 0){
-                visor.text = visor.text.subSequence(0,leght-leght)
-            }
-        }
 
-        igual.setOnClickListener {  }
-
-        num0.setOnClickListener { visor.append("0") }
-        num1.setOnClickListener { visor.append("1") }
-        num2.setOnClickListener { visor.append("2") }
-        num3.setOnClickListener { visor.append("3") }
-        num4.setOnClickListener { visor.append("4") }
-        num5.setOnClickListener { visor.append("5") }
-        num6.setOnClickListener { visor.append("6") }
-        num7.setOnClickListener { visor.append("7") }
-        num8.setOnClickListener { visor.append("8") }
-        num9.setOnClickListener { visor.append("9") }
-        parenteses.setOnClickListener { visor.append("()") }
-        virgula.setOnClickListener { visor.append(",") }
     }
 
 
